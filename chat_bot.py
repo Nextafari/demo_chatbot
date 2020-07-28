8d517fbc421339e25a90a37235e8e859c6ba3ee2
Nextafari <chinedue16@gmail.com>
2020-07-28 01:00:17 +0100

    Trained bot with my own personal data

--- chat_bot.py
+++ chat_bot.py
@@ -1,20 +1,38 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
-import os
+from datetime import date
+# from datetime import time
+# from datetime import datetime
+# import os
 
 # naming the bot
 bot = ChatBot("Demo_bot")
 
 trainer = ListTrainer(bot)
 
-# Variable holding the path to chatterbot_corpus files
-basepath = os.listdir("/home/emmanuel/Desktop/chatbot/chatterbot_corpus/data/english/")
+today = str(date.today())
+date_today = ("Today is " + today)
 
-for files in basepath:
-    # Opening the files in the basepath variable and reading them
-    # line by line to train the bot
-    data = open("/home/emmanuel/Desktop/chatbot/chatterbot_corpus/data/english/" + files, "r").readlines()
-    trainer.train(data)
+conversations = [
+    "Hello",
+    "Hi",
+    "How are you today ?",
+    "I am quite well, thank you.",
+    "What is today's date ?",
+    date_today,
+    "Are you sapien ?",
+    "No I am just a machine built by Emmanuel",
+]
+
+trainer.train(conversations)
+# Variable holding the path to chatterbot_corpus files
+# basepath = os.listdir("/home/emmanuel/Desktop/chatbot/chatterbot_corpus/data/english/")
+
+# for files in basepath:
+#     # Opening the files in the basepath variable and reading them
+#     # line by line to train the bot
+#     data = open("/home/emmanuel/Desktop/chatbot/chatterbot_corpus/data/english/" + files, "r").readlines()
+#     trainer.train(data)
 
 while True:
     # User input
